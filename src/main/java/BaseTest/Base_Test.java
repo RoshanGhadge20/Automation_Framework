460846cc37701828d3e38ff8b3d476dc9b389fa6
RoshanGhadge <roshanghadge568@gmail.com>
2024-09-30 17:16:24 +0530

    Implemented Base_Test & Config.properties files

--- src/main/java/BaseTest/Base_Test.java
+++ src/main/java/BaseTest/Base_Test.java
@@ -1,5 +1,45 @@
 package BaseTest;
 
-public class Base_Test {
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base_Test
+{
+	public static WebDriver driver;
+	public static Properties pr;
+
+	public Base_Test() throws IOException {
+		FileReader fin = new FileReader(
+				"E://Automation_Framework//POM//src//test//java//Configurations//config.properties");
+		pr = new Properties();
+		pr.load(fin);
+	}
+
+	public void Initialize() {
+		if (pr.getProperty("browser").equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if ((pr.getProperty("browser").equalsIgnoreCase("Chrome"))) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+			System.out.println("Driver object is not yet initialize");
+		}
+		
+		driver.get(pr.getProperty("URL"));
+		driver.manage().window().maximize();
+	
+	}
+
+	public void teardown() {
+		driver.manage().deleteAllCookies();
+		driver.quit();
+	}
 }
