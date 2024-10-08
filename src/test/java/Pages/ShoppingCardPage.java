55be327ff5d8d4ead067c388d3bb1469efd84f09
RoshanGhadge <roshanghadge568@gmail.com>
2024-10-08 21:40:27 +0530

    few changes are made into files

--- src/test/java/Pages/ShoppingCardPage.java
+++ src/test/java/Pages/ShoppingCardPage.java
@@ -7,7 +7,8 @@ import BaseTest.Base_Test;
 
 public class ShoppingCardPage extends Base_Test {
 
-	public ShoppingCardPage() throws IOException, FileNotFoundException {
+	public ShoppingCardPage() throws IOException, FileNotFoundException
+	{
 		super();
 	
 	}
--- src/test/java/Tests/ShoppingCardPageTest.java
+++ src/test/java/Tests/ShoppingCardPageTest.java
@@ -1,9 +1,17 @@
 package Tests;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import BaseTest.Base_Test;
 
 public class ShoppingCardPageTest extends Base_Test 
 {
+
+	public ShoppingCardPageTest() throws IOException, FileNotFoundException 
+	{
+		super();
+	}
 	
 
 }
