0f2a09223b1cb0bea6b93f00416200b0244f110b
RoshanGhadge <roshanghadge568@gmail.com>
2024-10-08 21:38:41 +0530

    Added ShoppingCardPage

---
+++ src/test/java/Pages/ShoppingCardPage.java
@@ -0,0 +1,15 @@
+package Pages;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import BaseTest.Base_Test;
+
+public class ShoppingCardPage extends Base_Test {
+
+	public ShoppingCardPage() throws IOException, FileNotFoundException {
+		super();
+	
+	}
+
+}
---
+++ src/test/java/Tests/ShoppingCardPageTest.java
@@ -0,0 +1,9 @@
+package Tests;
+
+import BaseTest.Base_Test;
+
+public class ShoppingCardPageTest extends Base_Test 
+{
+	
+
+}
