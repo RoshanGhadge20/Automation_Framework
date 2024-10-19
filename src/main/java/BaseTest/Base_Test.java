4e22cfb6ddc0999fa45720a1fe10d09a6d3e71aa
RoshanGhadge <roshanghadge568@gmail.com>
2024-10-19 17:15:21 +0530

    formatted code for base test class

--- src/main/java/BaseTest/Base_Test.java
+++ src/main/java/BaseTest/Base_Test.java
@@ -4,11 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base_Test
