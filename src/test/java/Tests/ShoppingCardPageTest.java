1c4157f7ec5137655e76ab8c4b138a959d944f81
RoshanGhadge <roshanghadge568@gmail.com>
2024-10-22 15:37:13 +0530

    added reporting functionality to shoppingcardpage and yourorderpage

--- src/test/java/Tests/ShoppingCardPageTest.java
+++ src/test/java/Tests/ShoppingCardPageTest.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.fail;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -26,7 +27,6 @@ public class ShoppingCardPageTest extends Base_Test {
 
 	@BeforeMethod
 	public void initiate_all() throws FileNotFoundException, IOException, InterruptedException {
-		active();
 		loginpage = new LoginPage();
 		dashboardpage = loginpage.do_login(pr.getProperty("username"), pr.getProperty("password"));
 		productdetailspage = new ProductDetailsPage();
@@ -35,11 +35,13 @@ public class ShoppingCardPageTest extends Base_Test {
 
 	@Test(priority = 1, description = "Verify title of page", groups = {"Sanity"})
 	public void Verify_title_of_page() {
+		testreports.test_details("Verify title of page");
 		shoppingcardpage.verify_title();
 	}
 
 	@Test(priority = 2, description = "Validate user lands on correct page", groups = {"Sanity"})
 	public void check_page() {
+		testreports.test_details("Validate user lands on correct page");
 		shoppingcardpage.verify_correct_page();
 	}
 
@@ -47,6 +49,7 @@ public class ShoppingCardPageTest extends Base_Test {
 	@Test(priority = 3,description ="Select a product and add it to the shopping cart", groups = {"Sanity", "Regression"}, enabled = false)
 	public void add_product() throws IOException, Exception
 	{
+		testreports.test_details("Select a product and add it to the shopping cart");
 		//dashboardpage.search_product(pr.getProperty("product"));
 		productdetailspage.add_to_shoppingcart();
 	}
@@ -54,6 +57,7 @@ public class ShoppingCardPageTest extends Base_Test {
 	@Test(priority = 4, groups = "Sanity", description = "fetching list of items added into shoppingcart")
 	public void list_of_items()
 	{
+		testreports.test_details("fetching list of items added into shoppingcart");
 		shoppingcardpage.list_of_items_addedinto_shopping_cart();
 	}
 	
@@ -61,12 +65,13 @@ public class ShoppingCardPageTest extends Base_Test {
 	@Test(priority = 5, groups = "Regression", description = "Proceed to checkout ")
 	public void do_checkout()
 	{
+		testreports.test_details("Verify Checkout Functionality");
 		shoppingcardpage.proceed_to_buy();
 	}
 	
-	@AfterMethod void quite_all() 
+	@AfterMethod void quite_all(ITestResult result) 
 	{
-		driver.quit();
+		testreports.getresult(result);
 	}
 
 }
--- src/test/java/Tests/YourOrdersPageTest.java
+++ src/test/java/Tests/YourOrdersPageTest.java
@@ -2,9 +2,11 @@ package Tests;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.ResultSet;
 
 import org.openqa.selenium.devtools.v127.systeminfo.model.VideoDecodeAcceleratorCapability;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,6 +19,7 @@ import Pages.YourOrdersPage;
 
 public class YourOrdersPageTest extends Base_Test
 {
+	
 	LoginPage loginpage;
 	DashboardPage dashboardpage;
 	ProductDetailsPage productdetailspage;
@@ -31,7 +34,6 @@ public class YourOrdersPageTest extends Base_Test
 	@BeforeMethod
 	public void initiate() throws FileNotFoundException, IOException, InterruptedException
 	{
-		active();
 		loginpage = new LoginPage();
 		dashboardpage =loginpage.do_login(pr.getProperty("username"), pr.getProperty("password"));
 		//productdetailspage = new DashboardPage().click_product(pr.getProperty("product"));
@@ -43,31 +45,35 @@ public class YourOrdersPageTest extends Base_Test
 	@Test(priority = 1, description ="Verify title of page", groups = "Sanity")
 	public void check_title()
 	{
+		testreports.test_details("Verify title of page");
 		yourorderspage.get_title_of_yourorderpage();
 	}
 	
 	@Test(priority = 2, description ="Validate user is correctly landed on Yours order page by validating text", groups = {"Sanity", "Regression"})
 	public void check_page()
 	{
+		testreports.test_details("Validate user is correctly landed on Yours order page by validating text");
 		Assert.assertTrue(yourorderspage.Verify_YourOrdersPage());
 	}
 	
 	@Test(priority = 3, description = "Verifying all list of options available on your orders page", groups = "Sanity")
 	public void get_yourorders_options() throws InterruptedException
 	{
+		testreports.test_details("Verifying all list of options available on your orders page");
 		yourorderspage.order_options();
 	}
 	
 	@Test(priority = 4, description = "Verify correct path gets shown in your orders page", groups = {"Sanity", "Regression"})
 	public void check_path() throws InterruptedException
 	{
+		testreports.test_details("Verify correct path gets shown in your orders page");
 		yourorderspage.validate_path();
 	}
 	
 	
 	@AfterMethod
-	public void quite()
+	public void quite(ITestResult result)
 	{
-		driver.quit();
+		testreports.getresult(result);
 	}
 }
