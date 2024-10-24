d2f9bda58cb19e1b2c6966ac59f6b5e52612b0d8
Roshan Ghadge <roshanghadge568@gmail.com>
2024-10-24 16:33:26 +0530

    Added takescreenshot utility file

---
+++ src/test/java/CommonUtilities/TakeScreenshot.java
@@ -0,0 +1,27 @@
+package CommonUtilities;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import BaseTest.Base_Test;
+
+public class TakeScreenshot extends Base_Test {
+	public TakeScreenshot() throws IOException, FileNotFoundException {
+		super();
+	}
+
+	public void Takescreenhots(WebDriver driver) throws IOException
+	{
+		TakesScreenshot ts= (TakesScreenshot) driver;
+		File source= ts.getScreenshotAs(OutputType.FILE);
+		File dest= new File("E:/Automation_Framework/POM//Screenshot/FailedTC.png");
+		FileUtils.copyFile(source, dest);
+		System.out.println("screenshot saved");
+	}
+}
