31724e5b032afdf12b5b2c29a516a246e29d092b
Roshan Ghadge <roshanghadge568@gmail.com>
2024-11-13 21:37:50 +0530

    Added JsonUtility

---
+++ src/test/java/CommonUtilities/JsonUtility.java
@@ -0,0 +1,19 @@
+package CommonUtilities;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import com.google.gson.JsonObject;
+
+import BaseTest.Base_Test;
+
+public class JsonUtility extends Base_Test {
+
+	public JsonUtility() throws IOException, FileNotFoundException {
+		super();
+	}
+
+	String jpath = "E://Automation_Framework//POM//Test_Data//logindetails.json";
+	//JSONPObject jobj = new JSONPObject(jpath,);
+	
+
+}
