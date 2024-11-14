1e2e4ad8375644de25ea127501b6526f65911ea4
Roshan Ghadge <roshanghadge568@gmail.com>
2024-11-14 16:42:55 +0530

    Implemented jsonutility

--- src/test/java/CommonUtilities/JsonUtility.java
+++ src/test/java/CommonUtilities/JsonUtility.java
@@ -1,19 +1,29 @@
 package CommonUtilities;
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import com.google.gson.JsonObject;
-
+import com.google.gson.JsonParser;
 import BaseTest.Base_Test;
 
-public class JsonUtility extends Base_Test {
+public class JsonUtility extends Base_Test 
+{
+	public JsonObject jobj;
+	String json_file_path = "E://Automation_Framework//POM//Test_Data//logindetails.json";
 
 	public JsonUtility() throws IOException, FileNotFoundException {
 		super();
 	}
 
-	String jpath = "E://Automation_Framework//POM//Test_Data//logindetails.json";
-	//JSONPObject jobj = new JSONPObject(jpath,);
-	
+	public void load_jsonfile(String json_file_path) throws FileNotFoundException {
+		FileReader fr = new FileReader(json_file_path);
+		jobj = JsonParser.parseReader(fr).getAsJsonObject();
+
+	}
+
+	public String getvaluebykey(String key) {
+		return jobj.get(key).getAsString();
+	}
 
 }
