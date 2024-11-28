6fcd02e2a3d3537d9566306ea2976cf258ff8e3f
Roshan_Ghadge <roshanghadge568@gmail.com>
2024-11-28 20:54:30 +0530

    Reformatted code for listners

--- src/test/java/Listners/Retry.java
+++ src/test/java/Listners/Retry.java
@@ -3,23 +3,19 @@ package Listners;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
-public class Retry implements IRetryAnalyzer 
-{
+public class Retry implements IRetryAnalyzer {
 
-	int count=0;
-	int max_retry=2;
-	
+	int count = 0;
+	int max_retry = 2;
+
 	@Override
-	public boolean retry(ITestResult result) 
-	{
-		if (!result.isSuccess() )
-		{
-		if (count<=max_retry)
-		{
-			count++;
-			System.out.println("Retrying Test:- "+ result.getName() + "Retry Time:- "+ this.count);
-			return true;
-		}
+	public boolean retry(ITestResult result) {
+		if (!result.isSuccess()) {
+			if (count <= max_retry) {
+				count++;
+				System.out.println("Retrying Test:- " + result.getName() + "Retry Time:- " + this.count);
+				return true;
+			}
 		}
 		return false;
 	}
