0f274f248ebe93f4292ffb06663facb3e3e7a49d
Roshan_Ghadge <roshanghadge568@gmail.com>
2024-11-28 20:53:14 +0530

    Reformatted code for Tests of dashboard / product / login

--- src/test/java/Tests/DashboardPageTest.java
+++ src/test/java/Tests/DashboardPageTest.java
@@ -17,90 +17,78 @@ import Pages.DashboardPage;
 import Pages.LoginPage;
 import ScreenRecording.CaptureVideo;
 
-public class DashboardPageTest extends Base_Test
-{
+public class DashboardPageTest extends Base_Test {
 
 	LoginPage loginpage;
 	DashboardPage dashboardpage;
 	CaptureVideo capturevideo;
-	
-	
-	public DashboardPageTest() throws IOException
-	{
+
+	public DashboardPageTest() throws IOException {
 		super();
 	}
-	
+
 	@BeforeMethod
-	public void initialize() throws IOException, InterruptedException
-	{
-		
+	public void initialize() throws IOException, InterruptedException {
+
 		dashboardpage = new LoginPage().do_login(pr.getProperty("username"), pr.getProperty("password"));
 		capturevideo = new CaptureVideo();
 	}
-	
-	
+
 	/**
 	 * Verify title of dashboard page
 	 */
-	@Test(priority = 1 , groups = {"Sanity"}, retryAnalyzer = Listners.Retry.class)
-	public void get_title()
-	{
+	@Test(priority = 1, groups = { "Sanity" }, retryAnalyzer = Listners.Retry.class)
+	public void get_title() {
 		testreports.test_details("Verify title of dashboard page");
 		dashboardpage.get_page_title();
 		logger.info("DashboardTest get_title executed");
 	}
-	
+
 	/**
 	 * Verify the apperance of logo & size on dashboard page
 	 */
-	@Test(priority = 2, groups = {"Sanity"})
-	public void check_logo()
-	{
+	@Test(priority = 2, groups = { "Sanity" })
+	public void check_logo() {
 		testreports.test_details("verify appearnace of logo in dashboard page");
 		Assert.assertEquals(true, dashboardpage.verify_dashboard_logo());
 		logger.info("DashboardTest check_logo executed");
 	}
-	
-	
+
 	/**
 	 * Verify all the navbar options from dashboard page as expected
 	 */
-	@Test(priority = 3, groups = {"Sanity"})
-	public void get_elements()
-	{
+	@Test(priority = 3, groups = { "Sanity" })
+	public void get_elements() {
 		testreports.test_details("Validate all navbar options in dashboard page");
 		dashboardpage.dash_pg_navbar();
 		logger.info("DashboardTest get_elements executed");
 	}
-	
+
 	/**
 	 * Check if user is able to search product on dashboard page
 	 */
-	@Test(priority = 4, groups = {"Regression"}, retryAnalyzer = Listners.Retry.class)
-	public void search_products()
-	{
-		//capturevideo.StartRecording();
+	@Test(priority = 4, groups = { "Regression" }, retryAnalyzer = Listners.Retry.class)
+	public void search_products() {
+		// capturevideo.StartRecording();
 		testreports.test_details("Verify user is able to search product");
 		dashboardpage.search_product(pr.getProperty("product"));
 		logger.info("DashboardTest search_products executed");
-		//capturevideo.StopRecording();
+		// capturevideo.StopRecording();
 	}
-	
+
 	/**
 	 * Validate profile details & all listing of options from sidebar
 	 */
-	@Test(priority = 5, groups = {"Sanity"})
-	public void check_user_name()
-	{
+	@Test(priority = 5, groups = { "Sanity" })
+	public void check_user_name() {
 		testreports.test_details("Verify user profile details name on all section");
 		Assert.assertEquals(true, dashboardpage.verify_profile_details());
 		logger.info("DashboardTest check_user_name executed");
 	}
-	
+
 	@AfterMethod
-	public void tdown(ITestResult result)
-	{
-	testreports.getresult(result);	
+	public void tdown(ITestResult result) {
+		testreports.getresult(result);
 	}
-	
+
 }
