56abd323b2b302e7dcb04c176199b36333df6a0f
Roshan_Ghadge <roshanghadge568@gmail.com>
2024-12-01 18:12:49 +0530

    Reformatted code of Screenrecordingutility

--- src/test/java/ScreenRecording/CaptureVideo.java
+++ src/test/java/ScreenRecording/CaptureVideo.java
@@ -9,28 +9,25 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 import java.security.PublicKey;
 import java.util.concurrent.TimeUnit;
 
-public class CaptureVideo
-{
- 
-	public void StartRecording() 
-	{
+public class CaptureVideo {
+
+	public void StartRecording() {
 		// start screen recording
 		try {
 			ScreenRecorderUtil.startRecord("main");
 		} catch (Exception e) {
-			System.out.println("Unable to start recording "+ e.getMessage());
+			System.out.println("Unable to start recording " + e.getMessage());
 		}
 	}
-	   
-	public void StopRecording()
-	{
-      // stop recording
-      try {
-		ScreenRecorderUtil.stopRecord();
-	} catch (Exception e) {
-		   System.out.println("Unable to stop recording "+ e.getMessage());
-		e.printStackTrace();
-	}
-   
+
+	public void StopRecording() {
+		// stop recording
+		try {
+			ScreenRecorderUtil.stopRecord();
+		} catch (Exception e) {
+			System.out.println("Unable to stop recording " + e.getMessage());
+			e.printStackTrace();
+		}
+
 	}
 }
