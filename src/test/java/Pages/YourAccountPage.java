e7af4a0694df4a766b3b91132325b9a36c59db24
Roshan_Ghadge <roshanghadge568@gmail.com>
2024-12-22 17:41:39 +0530

    Reformatted code for yourorderspage & test

--- src/test/java/Pages/YourAccountPage.java
+++ src/test/java/Pages/YourAccountPage.java
@@ -2,69 +2,54 @@ package Pages;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
-
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import com.mysql.cj.util.TestUtils;
-
 import BaseTest.Base_Test;
 import CommonUtilities.Testutils;
-import io.github.bonigarcia.wdm.managers.VoidDriverManager;
 
-public class YourAccountPage extends Base_Test
-{
-	
-	Testutils testutils= new Testutils();
+public class YourAccountPage extends Base_Test {
+
+	Testutils testutils = new Testutils();
 
 	public YourAccountPage() throws IOException, FileNotFoundException {
 		super();
-	PageFactory.initElements(driver, this);	
+		PageFactory.initElements(driver, this);
 	}
 
-	
-	//Object Pool of Webelements
-	
+	// Object Pool of Webelements
+
 	@FindBy(xpath = "//a[@aria-labelledby='Account & Lists']")
 	WebElement YourAccount;
-	
+
 	@FindBy(xpath = "//h1[contains(text(),' Your Account')]")
 	WebElement PageText;
-	
+
 	@FindBy(css = "div.a-box-inner")
 	List<WebElement> Youraccount_options;
-	
-	// Test Methods 
-	
-	public void navigate_to_youraccount()
-	{
+
+	// Test Methods
+
+	public void navigate_to_youraccount() {
 		YourAccount.click();
 	}
-	
-	public WebElement check_page(){
+
+	public WebElement check_page() {
 		YourAccount.click();
 		testutils.wait.until(ExpectedConditions.visibilityOf(PageText));
 		return PageText;
 	}
-	
-	public void list_of_options()
-	{
-		int i=1;
+
+	public void list_of_options() {
+		int i = 1;
 		YourAccount.click();
 		testutils.wait.until(ExpectedConditions.visibilityOf(PageText));
-		for(WebElement ele: Youraccount_options)
-		{
-			System.out.println("Options in Your Account Page"+ele.getText()+i);
+		for (WebElement ele : Youraccount_options) {
+			System.out.println("Options in Your Account Page" + ele.getText() + i);
 			i++;
 		}
 	}
-	
-	
-	
-	
+
 }
--- src/test/java/Tests/YourAccountPageTest.java
+++ src/test/java/Tests/YourAccountPageTest.java
@@ -2,54 +2,42 @@ package Tests;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.security.PublicKey;
-
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import BaseTest.Base_Test;
-import Pages.DashboardPage;
 import Pages.LoginPage;
 import Pages.YourAccountPage;
 
-public class YourAccountPageTest extends Base_Test
-{
+public class YourAccountPageTest extends Base_Test {
 	LoginPage loginpage;
 	YourAccountPage youraccountpage;
-	
-	public YourAccountPageTest() throws IOException
-	{
+
+	public YourAccountPageTest() throws IOException {
 		super();
 	}
-	
+
 	@BeforeMethod
-	public void initialize() throws FileNotFoundException, IOException, InterruptedException
-	{
+	public void initialize() throws FileNotFoundException, IOException, InterruptedException {
 		youraccountpage = new LoginPage().logining(pr.getProperty("username"), pr.getProperty("password"));
-		
+
 	}
-	
+
 	@Test(priority = 1, groups = {"Sanity"})
-	public void check_title()
-	{
+	public void check_title() {
 		testreports.test_details("Verify Page Title on your account page");
 		System.out.println(youraccountpage.check_page());
 	}
-	
+
 	@Test(priority = 2, groups = {"Sanity"})
-	public void check_options()
-	{
+	public void check_options() {
 		testreports.test_details("Verify options on your account page");
 		youraccountpage.list_of_options();
 	}
-	
-	
-	
+
 	@AfterMethod
-	public void teardown(ITestResult result)
-	{
+	public void teardown(ITestResult result) {
 		testreports.getresult(result);
 	}
 }
