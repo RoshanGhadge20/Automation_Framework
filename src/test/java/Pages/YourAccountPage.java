37d8cad6bc0ec576e2221bf90cc6061f63decb41
RoshanGhadge <roshanghadge568@gmail.com>
2024-12-29 17:38:54 +0530

    Reformatted code for the Youraccountpage & test

--- src/test/java/Pages/YourAccountPage.java
+++ src/test/java/Pages/YourAccountPage.java
@@ -1,19 +1,17 @@
 package Pages;
 
-import java.awt.RenderingHints.Key;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap.KeySetView;
 
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import BaseTest.Base_Test;
 import CommonUtilities.Testutils;
 
@@ -36,11 +34,9 @@ public class YourAccountPage extends Base_Test {
 
 	@FindBy(css = "div.a-box-inner")
 	List<WebElement> Youraccount_options;
-	
+
 	@FindBy(css = "a.ya-card__whole-card-link")
 	List<WebElement> linksElements;
-	
-	
 
 	// Actions class
 	Actions act = new Actions(driver);
@@ -66,50 +62,46 @@ public class YourAccountPage extends Base_Test {
 		}
 	}
 
-	public void verify_redirection_of_links() throws IOException
-	{
+	public void verify_redirection_of_links() throws IOException {
 		YourAccount.click();
 		testutils.wait.until(ExpectedConditions.visibilityOf(PageText));
-		for (WebElement element: linksElements) 
-		{
-			//act.keyDown(Keys.CONTROL).click(element).build().perform();
-			
+		for (WebElement element : linksElements) {
+			// act.keyDown(Keys.CONTROL).click(element).build().perform();
+
 			String href = element.getAttribute("href");
-			
+
 			if (href == null || href.isEmpty()) {
-                System.out.println("Skipping link: href is null or empty");
-                continue; // Skip invalid links
-            }
-			
-			try {
-				
-			 URL url = new URL(href);
-
-	            // Open an HTTP connection to the URL
-	            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-	            // Set request method to HEAD for faster response
-	            connection.setRequestMethod("HEAD");
-
-	            // Connect to the URL
-	            connection.connect();
-
-	            // Get the response code
-	            int responseCode = connection.getResponseCode();
-
-	            // Print or log the response code
-	            if (responseCode >= 200 && responseCode < 300) {
-	                System.out.println(href + " is valid with response code: " + responseCode);
-	            } else {
-	                System.out.println(href + " is broken with response code: " + responseCode);
-	            }
-	            
+				System.out.println("Skipping link: href is null or empty");
+				continue; // Skip invalid links
 			}
-	            catch (Exception e) {
-	            		System.out.println("Exception Caught "+ e.getMessage());
+
+			try {
+
+				URL url = new URL(href);
+
+				// Open an HTTP connection to the URL
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+				// Set request method to HEAD for faster response
+				connection.setRequestMethod("HEAD");
+
+				// Connect to the URL
+				connection.connect();
+
+				// Get the response code
+				int responseCode = connection.getResponseCode();
+
+				// Print or log the response code
+				if (responseCode >= 200 && responseCode < 300) {
+					System.out.println(href + " is valid with response code: " + responseCode);
+				} else {
+					System.out.println(href + " is broken with response code: " + responseCode);
 				}
+
+			} catch (Exception e) {
+				System.out.println("Exception Caught " + e.getMessage());
+			}
 		}
-	
 
 	}
 }
--- src/test/java/Tests/YourAccountPageTest.java
+++ src/test/java/Tests/YourAccountPageTest.java
@@ -3,11 +3,11 @@ package Tests;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.openqa.selenium.devtools.v127.systeminfo.model.VideoDecodeAcceleratorCapability;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import BaseTest.Base_Test;
 import Pages.LoginPage;
 import Pages.YourAccountPage;
@@ -26,25 +26,24 @@ public class YourAccountPageTest extends Base_Test {
 
 	}
 
-	@Test(priority = 1, groups = {"Sanity"})
+	@Test(priority = 1, groups = { "Sanity" })
 	public void check_title() {
 		testreports.test_details("Verify Page Title on your account page");
 		System.out.println(youraccountpage.check_page());
 	}
 
-	@Test(priority = 2, groups = {"Sanity"})
+	@Test(priority = 2, groups = { "Sanity" })
 	public void check_options() {
 		testreports.test_details("Verify options on your account page");
 		youraccountpage.list_of_options();
 	}
 
-	@Test(priority = 3, groups = {"Regression"})
-	public void check_links() throws IOException
-	{
+	@Test(priority = 3, groups = { "Regression" })
+	public void check_links() throws IOException {
 		testreports.test_details("Checking links on page");
 		youraccountpage.verify_redirection_of_links();
 	}
-	
+
 	@AfterMethod
 	public void teardown(ITestResult result) {
 		testreports.getresult(result);
